9bfe700422cb41cc1a1441ed3eada1863ce1586c
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-10-26 18:46:24 +0530

    added

--- app/src/pages/Add.tsx
+++ app/src/pages/Add.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import { addMovie } from "../services/api";
@@ -6,11 +7,14 @@ import Layout from "../components/layout";
 function Add() {
   const navigate = useNavigate();
 
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieYear, setMovieYear] = useState("");
+
   async function addThisDummyMovie() {
     try {
       const moviePayload = {
-        title: "My dummy movie",
-        year: 1998,
+        title: movieTitle,
+        year: parseInt(movieYear),
       };
       const response = await addMovie(moviePayload);
       console.log(response);
@@ -27,12 +31,20 @@ function Add() {
         <h1>Add</h1>
         <form>
           <label>Enter Movie Name</label>
-          <input type="text"></input>
+          <input
+            type="text"
+            value={movieTitle}
+            onChange={(e) => setMovieTitle(e.target.value)}
+          />
           <label>Enter Movie Year</label>
-          <input type="text"></input>
+          <input
+            type="number"
+            value={movieYear}
+            onChange={(e) => setMovieYear(e.target.value)}
+          />
         </form>
 
-        <button onClick={() => addThisDummyMovie()}>add dummy movie</button>
+        <button onClick={addThisDummyMovie}>Add Dummy Movie</button>
 
         <Link to="/">
           <button>Submit</button>
